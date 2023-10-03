0bc1c2d5b3483e75c4c934b5d50797715714ddf1
Dan <35927536+dan-mba@users.noreply.github.com>
2023-10-03 09:46:06 -0400

    setup form

---
+++ src/components/RepoForm.tsx
@@ -0,0 +1,24 @@
+export default function RepoForm() {
+  return (
+    <form className="flex justify-center gap-2">
+      <div className="flex flex-col w-[35ch]">
+        <label htmlFor="org" className="pb-1">Repo Owner</label>
+        <input name="org" id="org" type="text" pattern="[A-Za-z0-9_.\-]+"
+          className="text-black invalid:border-pink-500 invalid:text-pink-600"
+        />
+      </div>
+      <div className="flex flex-col w-[35ch]">
+        <label htmlFor="repo" className="pb-1">Repo Name</label>
+        <input name="repo" id="repo" type="text" pattern="[A-Za-z0-9_.\-]+"
+          className="text-black invalid:border-pink-500 invalid:text-pink-600"
+        />
+      </div>
+      <div className="flex flex-col w-[25ch]">
+        <label htmlFor="user" className="pb-1">GitHub ID</label>
+        <input name="user" id="user" type="text" pattern="[A-Za-z0-9_.\-]+"
+          className="text-black invalid:border-pink-500 invalid:text-pink-600"
+        />
+      </div>
+    </form>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import forms from '@tailwindcss/forms'
 
 const config: Config = {
   content: [
@@ -15,6 +16,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    forms,
+  ],
 }
 export default config
